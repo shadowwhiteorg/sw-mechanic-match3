edf8d3c6444b3faa31c6633a595412fa0ec0fee7
ww-org <semir@shadowwhite.org>
2025-05-06 13:48:12 +0300

    Feat: Event System

---
+++ UnityProject/Assets/_Game/Scripts/Core/Events/GameEvents.cs
@@ -0,0 +1,10 @@
+﻿using _Game.Interfaces;
+
+namespace _Game.Core.Events
+{
+    public class GameStartedEvent : IGameEvent
+    {
+        public string Message { get; }
+        public GameStartedEvent(string message) => Message = message;
+    }
+}
