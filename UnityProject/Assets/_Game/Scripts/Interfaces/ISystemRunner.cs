c174c315b58939289883005334f88c6cb7fda2bf
ww-org <semir@shadowwhite.org>
2025-05-06 14:48:56 +0300

    Feat: System Runner

---
+++ UnityProject/Assets/_Game/Scripts/Interfaces/ISystemRunner.cs
@@ -0,0 +1,12 @@
+﻿using _Game.Scripts.Interfaces;
+
+namespace _Game.Core
+{
+    public interface ISystemRunner
+    {
+        void Register(IUpdatableSystem system);
+        void Register(IFixedUpdatableSystem system);
+        void Tick();
+        void FixedTick();
+    }
+}
---
+++ UnityProject/Assets/_Game/Scripts/Interfaces/IUpdatableSystem.cs
@@ -0,0 +1,7 @@
+﻿namespace _Game.Scripts.Interfaces
+{
+    public interface IUpdatableSystem
+    {
+        void Tick();
+    }
+}
