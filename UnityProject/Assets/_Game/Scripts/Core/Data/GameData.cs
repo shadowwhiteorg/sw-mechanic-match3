2d7b29f6e903aa246e828ce11e6555f0cd7a2a55
ww-org <semir@shadowwhite.org>
2025-05-06 14:10:04 +0300

    Feat: Save/Load System

---
+++ UnityProject/Assets/_Game/Scripts/Core/Data/GameData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using _Game.Systems.Core.Data;
+
+namespace _Game.Systems.FinanceSystem
+{
+    [Serializable]
+    public class GameData
+    {
+        public string PlayerName;
+        public int Level;
+        public int Score;
+        public  List<ScoreEntry> Scores = new List<ScoreEntry>();
+        
+    }
+}
