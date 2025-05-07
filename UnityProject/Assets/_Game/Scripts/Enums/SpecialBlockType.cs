f8ba3f90309951ccec00c00bb49ecfbeb5c2f811
ww-org <semir@shadowwhite.org>
2025-05-07 11:52:00 +0300

    Feat: Special behavior core

---
+++ UnityProject/Assets/_Game/Scripts/Enums/SpecialBlockType.cs
@@ -0,0 +1,11 @@
+﻿namespace _Game.Enums
+{
+    public enum SpecialBlockType
+    {
+        None = 0, 
+        Bomb = 1,
+        Rocket = 2,
+        Duck = 3 
+        /* …plus future ones*/
+    }
+}
---
+++ UnityProject/Assets/_Game/Scripts/Interfaces/ISpecialBlockBehavior.cs
@@ -0,0 +1,12 @@
+﻿using _Game.Enums;
+using _Game.Systems.BlockSystem;
+using _Game.Systems.GridSystem;
+
+namespace _Game.Interfaces
+{
+    public interface ISpecialBlockBehavior
+    {
+        SpecialBlockType HandledType { get; }
+        void Activate(BlockModel block, GridHandler grid = null);
+    }
+}
